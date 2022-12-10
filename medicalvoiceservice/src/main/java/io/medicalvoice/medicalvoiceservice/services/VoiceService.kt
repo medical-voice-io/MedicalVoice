9b5d73fc270c1b4b0ecfd4e8c99af7fa314be13c
ShiryaevNikolay <shiryaev@surfstudio.ru>
2022-12-10 10:24:35 +0300

    #7 - рефакторинг VoiceService

--- medicalvoiceservice/src/main/java/io/medicalvoice/medicalvoiceservice/services/VoiceService.kt
+++ medicalvoiceservice/src/main/java/io/medicalvoice/medicalvoiceservice/services/VoiceService.kt
@@ -1,23 +1,18 @@
 package io.medicalvoice.medicalvoiceservice.services
 
-import android.app.Service
 import android.content.Intent
 import android.os.IBinder
 import android.util.Log
+import io.medicalvoice.medicalvoiceservice.R
+import io.medicalvoice.medicalvoiceservice.domain.NotificationData
 import io.medicalvoice.medicalvoiceservice.services.binders.MedicalVoiceBinder
 import io.medicalvoice.medicalvoiceservice.services.events.Event
 import io.medicalvoice.medicalvoiceservice.services.events.StopRecordingEvent
-import io.medicalvoice.medicalvoiceservice.services.extensions.startForegroundAndShowNotification
-import io.medicalvoice.medicalvoiceservice.services.extensions.stopForeground
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.shareIn
 import kotlinx.coroutines.launch
-import kotlin.coroutines.CoroutineContext
 
 /**
  * Сервис для записи аудио из микрофона в фоновом режиме
@@ -28,9 +23,18 @@ class VoiceService(
     private val audioRecorderInteractor: AudioRecorderInteractor = AudioRecorderInteractor(
         audioRecorderRepository = AudioRecorderRepository()
     )
-) : Service(), CoroutineScope {
+) : BaseNotificationService() {
 
-    override val coroutineContext: CoroutineContext = SupervisorJob() + Dispatchers.IO
+    override val appPackageName: String = APP_PACKAGE_NAME
+    override val notificationData: NotificationData by lazy {
+        NotificationData(
+            channelId = VOICE_NOTIFICATION_CHANNEL_ID,
+            channelName = VOICE_NOTIFICATION_CHANNEL_NAME,
+            title = resources.getString(R.string.notification_title),
+            text = resources.getString(R.string.notification_text),
+            smallIconRes = R.drawable.ic_android
+        )
+    }
 
     private val _audioRecordingEventFlow = MutableSharedFlow<Event>()
 
@@ -46,16 +50,16 @@ class VoiceService(
             audioRecorderInteractor.audioRecordingEventFlow
                 .shareIn(this, started = SharingStarted.Eagerly, replay = 1)
                 .collect { event ->
-                    if (event is StopRecordingEvent) stopService()
+                    if (event is StopRecordingEvent) stopSelf()
                     _audioRecordingEventFlow.emit(event)
                 }
         }
     }
 
-    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
-        Log.i(TAG, "$TAG onStartCommand")
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        super.onStartCommand(intent, flags, startId)
 
-        startForegroundAndShowNotification()
+        Log.i(TAG, "$TAG onStartCommand")
 
         launch(coroutineContext) {
             audioRecorderInteractor.startRecording()
@@ -88,16 +92,13 @@ class VoiceService(
         launch(coroutineContext) {
             audioRecorderInteractor.stopRecording()
         }
-        stopService()
         super.onDestroy()
-    }
-
-    private fun stopService() {
-        stopForeground()
-        stopSelf()
     }
 
     private companion object {
         const val TAG = "MedicalVoiceService"
+        const val VOICE_NOTIFICATION_CHANNEL_ID = "VOICE_NOTIFICATION_CHANNEL_ID"
+        const val VOICE_NOTIFICATION_CHANNEL_NAME = "Foreground MedicalVoice Service"
+        const val APP_PACKAGE_NAME = "io.medicalvoice.android.MainActivity"
     }
 }
