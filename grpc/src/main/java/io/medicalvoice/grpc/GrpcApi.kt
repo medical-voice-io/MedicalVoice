b7b586561ed7ed07f294d38d6b5bf8baf9908e06
ShiryaevNikolay <shiryaev@surfstudio.ru>
2023-02-10 19:38:06 +0300

    #1 - тестовый класс для взаимодействия с каналом связи

--- grpc/src/main/java/io/medicalvoice/grpc/GrpcApi.kt
+++ grpc/src/main/java/io/medicalvoice/grpc/GrpcApi.kt
@@ -1,4 +1,23 @@
 package io.medicalvoice.grpc
 
-class GrpcApi {
+import io.grpc.ManagedChannel
+import io.medicalvoice.grpc.proto.MedicalVoiceApiGrpcKt.MedicalVoiceApiCoroutineStub
+import io.medicalvoice.grpc.proto.VoiceRequest
+import kotlinx.coroutines.flow.Flow
+import java.io.Closeable
+import java.util.concurrent.TimeUnit
+
+class MedicalVoiceClient(
+    private val channel: ManagedChannel
+) : Closeable {
+
+    private val stub: MedicalVoiceApiCoroutineStub = MedicalVoiceApiCoroutineStub(channel)
+
+    suspend fun myFunc(voiceData: Flow<VoiceRequest>) {
+        stub.sendVoiceData(voiceData)
+    }
+
+    override fun close() {
+        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS)
+    }
 }
